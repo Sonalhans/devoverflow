a0d754fd0f1297b4affc0fe5b0924cc4b156fe31
Sonal Hans <sonalhans05@gmail.com>
2024-04-17 22:36:50 +0530

    Ask Question page with editor and form

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -1,3 +1,15 @@
-export default function AskQuestion() {
-  return <h1>ASK QUESTION</h1>;
-}
+import Question from "@/components/forms/Question";
+import React from "react";
+
+const Page = () => {
+  return (
+    <div>
+      <h1 className="h1-bold text-dark100_light900">Ask a Question</h1>
+      <div className="mt-9 ">
+        <Question />
+      </div>
+    </div>
+  );
+};
+
+export default Page;
