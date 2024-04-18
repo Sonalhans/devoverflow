109b9339ce10cbee293ee78aaa1dfa289a114d95
Sonal Hans <sonalhans05@gmail.com>
2024-04-18 16:29:45 +0530

    Created Question ,Tags and User Model

--- components/forms/Question.tsx
+++ components/forms/Question.tsx
@@ -133,20 +133,15 @@ const Question = () => {
                     height: 350,
                     menubar: false,
                     plugins:
-                      "anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount checklist mediaembed casechange export formatpainter pageembed linkchecker a11ychecker tinymcespellchecker permanentpen powerpaste advtable advcode editimage advtemplate ai mentions tinycomments tableofcontents footnotes mergetags autocorrect typography inlinecss markdown",
+                      "anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount checklist mediaembed casechange export formatpainter pageembed linkchecker a11ychecker tinymcespellchecker permanentpen powerpaste advtable advcode editimage advtemplate mentions tinycomments tableofcontents footnotes mergetags autocorrect typography inlinecss markdown",
                     toolbar:
-                      "undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | addcomment showcomments | spellcheckdialog a11ycheck typography | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat",
+                      "undo redo |fontsize | bold italic underline strikethrough |  align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat",
                     tinycomments_mode: "embedded",
                     tinycomments_author: "Author name",
                     mergetags_list: [
                       { value: "First.Name", title: "First Name" },
                       { value: "Email", title: "Email" },
                     ],
-
-                    ai_request: (request, respondWith) =>
-                      respondWith.string(() =>
-                        Promise.reject("See docs to implement AI Assistant")
-                      ),
                   }}
                   initialValue=""
                   onBlur={field.onBlur}
